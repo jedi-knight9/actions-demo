e84e37dd4014d3dd3abd5022ba26cb90a98cda3e
jedi-knight9 <jediknightnow@gmail.com>
2024-11-13 08:57:38 -0800

    add index.js

---
+++ index.js
@@ -0,0 +1,6 @@
+const button = document.getElementById("showMessageButton");
+const message = document.getElementById("message");
+
+button.addEventListener("click", () => {
+  message.style.display = "block";
+});
